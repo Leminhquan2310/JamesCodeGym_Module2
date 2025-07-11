3a2b2cd78aabef57f9f191d18436952811c20720
minhquan <quanleminh2310@gmail.com>
2025-07-11 17:19:04 +0700

    add Variable_DataType project

---
+++ Variable_DataType/src/VariableDataType.java
@@ -0,0 +1,16 @@
+public class VariableDataType {
+    public static void main(String[] args) {
+        int i = 10; // kiểu int giá trị 10
+        float f = 20.5f; // kiểu float có giá trị 20.5
+        double d = 20.5; // kiểu double có giá trị 20.5
+        boolean b = true; // kiểu boolean có giá trị true
+        char c = 'a'; // kiểu char char có giá trị là 'a'
+        String s = "Hà nội"; // kiểu String có giá trị là "Hà nội"
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+    }
+}
