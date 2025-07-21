2fe5dcd755d9cd3a3013dbd8bb1709f475e7a140
minhquan <quanleminh2310@gmail.com>
2025-07-21 20:21:26 +0700

    Add Only_Set project

---
+++ Only_Set/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        System.out.println(student1.toString());
+        student1.setName("Quân");
+        student1.setClasses("C04");
+        System.out.println(student1.toString());
+
+    }
+}
---
+++ Only_Set/src/Student.java
@@ -0,0 +1,25 @@
+public class Student {
+    private String name = "Join";
+    private String classes = "C02";
+
+
+    public Student() {
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", classes='" + classes + '\'' +
+                '}';
+    }
+}
