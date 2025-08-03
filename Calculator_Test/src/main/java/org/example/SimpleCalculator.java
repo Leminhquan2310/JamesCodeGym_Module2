04427f04031f5fa5a87ac9aaf35ea7448399de2b
minhquan <quanleminh2310@gmail.com>
2025-08-03 13:07:38 +0700

    Add CalculatorTest maven project

---
+++ Calculator_Test/src/main/java/org/example/SimpleCalculator.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class SimpleCalculator {
+    public static int add(int first, int second){
+        throw new UnsupportedOperationException();
+    }
+
+    public static int sub(int first, int second){
+        throw new UnsupportedOperationException();
+    }
+}
