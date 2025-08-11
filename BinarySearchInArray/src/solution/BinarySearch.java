a95fc5b98084d29441c01ff191edca11db1da6ec
minhquan <quanleminh2310@gmail.com>
2025-08-11 14:06:40 +0700

    Add BinarySearchInArray project

---
+++ BinarySearchInArray/src/solution/BinarySearch.java
@@ -0,0 +1,27 @@
+package solution;
+
+public class BinarySearch {
+    static int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+
+    public static void main(String[] args) {
+        System.out.println(binarySearch(list, 2));  /* 0 */
+        System.out.println(binarySearch(list, 11)); /* 4 */
+        System.out.println(binarySearch(list, 79)); /*12 */
+        System.out.println(binarySearch(list, 1));  /*-1 */
+        System.out.println(binarySearch(list, 5));  /*-1 */
+        System.out.println(binarySearch(list, 80)); /*-1 */
+    }
+
+
+    public static int binarySearch(int[] list, int key) {
+        int left = 0;
+        int right = list.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (list[mid] < key ) left = mid +1;
+            else if (list[mid] > key) right = mid - 1;
+            else return mid;
+        }
+        return -1;
+    }
+}
