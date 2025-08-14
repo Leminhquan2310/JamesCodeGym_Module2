2beba518459ddb96f84f760af714fe7aaa8861fd
minhquan <quanleminh2310@gmail.com>
2025-08-14 12:11:26 +0700

    Add selection_sort solution

---
+++ Sorting_Algorithm/src/main/java/solution/selection_sort/SelectionSort.java
@@ -0,0 +1,26 @@
+package solution.selection_sort;
+
+public class SelectionSort {
+    static double[] list = {1, 9, 4.5, 6.6, 5.7, -4.5};
+
+    public static void main(String[] args) {
+        selectionSort(list);
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + "\t");
+        }
+    }
+
+    public static void selectionSort (double[] list) {
+        for (int i = 0; i < list.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < list.length; j++) {
+                if (list[j] < list[min]) {
+                    min = j;
+                }
+            }
+            double temp = list[i];
+            list[i] = list[min];
+            list[min] = temp;
+        }
+    }
+}
